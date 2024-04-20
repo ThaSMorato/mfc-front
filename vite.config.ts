a87de821d251cbb16e7bb766933adfca65f9683a
ThaSMorato <tha.s.morato@gmail.com>
2024-04-20 00:36:04 -0300

    Chore: tests configs

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,16 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:8080',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,8 +4,11 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    environment: 'jsdom',
     globals: true,
+    setupFiles: ['./test/setup.ts'],
     root: './',
+    include: ['**/*.spec.ts', '**/*.spec.tsx'],
   },
   plugins: [
     tsConfigPaths(),
